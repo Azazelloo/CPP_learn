08296b72d0e57219612fcbf574afc73998024ba9
Azazelloo <smokenaces@mail.ru>
2021-02-26 16:32:38 +0300

    1,2,3

--- Solutions/Procedure/lab_4/other.cpp
+++ Solutions/Procedure/lab_4/other.cpp
@@ -1,7 +1,53 @@
-//#include "other.h"
+#include "other.h"
 
 ///////////////////////////////////////////////////
 
+int IncByValue(int val) {
+	return ++val;
+	}
+
+void IncByPointer(int* val) {
+	(*val)++;
+	}
+
+void IncByReference(int& val) {
+	val++;
+	}
+
+
+void Swap(int* pVal1, int* pVal2) {
+	int tmpVal = *pVal1;
+	*pVal1 = *pVal2;
+	*pVal2 = tmpVal;
+	}
+
+void Swap(int& rVal1, int& rVal2) {
+	int tmpVal = rVal1;
+	rVal1 = rVal2;
+	rVal2 = tmpVal;
+	}
+
+
+void PrintArray(int* pArray,const int& rowSize,const int& colSize) {
+	for (int i = 0; i < rowSize;i++) {
+		for (int j = 0; j < colSize;j++) {
+			std::cout << pArray[j + colSize * i]<<" ";
+			}
+		std::cout << std::endl;
+		}
+	}
+
+void PrintArray() {
+
+	}
+
+int Min(int* pArray, const int& rowSize, const int& colSize) {
+	int min = pArray[0];
+	for (int i = 1; i < rowSize*colSize;i++) {
+		if (pArray[i] < min) min = pArray[i];
+		}
+	return min;
+	}
 
 
 //Подсказка-заготовка для задания 5а
--- Solutions/Procedure/lab_4/other.h
+++ Solutions/Procedure/lab_4/other.h
@@ -1,3 +1,25 @@
-
+#pragma once
 //Прототипы используемых в данном задании функций:
 
+#include <iostream>
+#include <tchar.h>
+//#include <cstdio>
+//#include <cstdarg>
+
+
+
+#define	  stop __asm nop
+
+extern double& ref;
+
+int IncByValue(int val);
+void IncByPointer(int* val);
+void IncByReference(int& val);
+
+void Swap(int* pVal1, int* pVal2);
+void Swap(int& rVal1,int& rVal2);
+
+void PrintArray(int* pArray,const int& rowSize,const int& colSize);
+
+
+int Min(int* pArray, const int& rowSize, const int& colSize);
